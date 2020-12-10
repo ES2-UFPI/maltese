f35368c0d40a428177307b02b219ddab0313e984
Alexander <alex.oliveira76@hotmail.com>
2020-12-10 19:45:50 -0300

    creating backend server and connecting to the database

--- maltese-backend/src/main.js
+++ maltese-backend/src/main.js
@@ -1,1 +1,15 @@
-console.log("Hello, world!");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const uri = "mongodb+srv://maltese:ufpi@maltese.hwddu.mongodb.net/testes?retryWrites=true&w=majority"
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3333, () => console.log("Servidor executando na porta 3333..."));
