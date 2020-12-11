075ae3bf5e85d409a9a2c8801d9b60815ef1d0ef
Natanael Oliveira <ntnaoliveira@gmail.com>
2020-12-11 19:47:01 -0300

    adding routes

--- maltese-backend/src/routes.js
+++ maltese-backend/src/routes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
 const ClientControllers = require("./controllers/ClientControllers");
+const ProductContollers = require("./controllers/ProductControllers");
 
 const routes = express.Router();
 
 routes.get("/clients/:provider_id", ClientControllers.showProductsStore);
+
+routes.post("/Product", ProductContollers.store);
+routes.get("/Product", ProductContollers.index);
+routes.put("/Product/:product_id", ProductContollers.update);
+routes.delete("/Product/:product_id", ProductContollers.delete);
 
 module.exports = routes;
