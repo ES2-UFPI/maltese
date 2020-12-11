ff38c3e0bf5926d28a94dde93b2d2818cc07a188
Natanael Oliveira <ntnaoliveira@gmail.com>
2020-12-11 19:45:48 -0300

    adding product's CRUD

--- maltese-backend/src/controllers/ProductControllers.js
+++ maltese-backend/src/controllers/ProductControllers.js
@@ -1,3 +1,45 @@
 const Product = require("../models/Product");
 
-module.exports = {};
+module.exports = {
+    //create
+    async store (req, res){
+        const{name, price} = req.body;
+
+        let product = await Product.findOne({name});
+
+        if(!product){
+            product = await Product.create({name, price});
+        }
+
+        return res.status(201).json(product);
+    },
+    //Read
+    async index(req, res){
+        const products = await Product.find();
+
+        return res.json(products);
+    },
+    
+    async update(req, res){
+        const{product_id} = req.params;
+        const{price} = req.body;
+
+        let product = await Product.findByIdAndUpdate(product_id,{price},{new:true});
+
+        if(!product) return res.status(400).json({error: "Product not found"});
+
+        return res.json(product);
+    },
+
+    async delete(req, res){
+        const {product_id} = req.params;
+
+        let product = await Product.findById(product_id);
+
+        if(!product) return res.status(400).json({  error:  "product not found" });
+
+        await product.delete();
+
+        return res.sendStatus(204);
+    }
+};
