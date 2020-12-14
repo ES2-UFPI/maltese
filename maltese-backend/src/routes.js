e2d8b8117740263abf25143c587de6c935364d0c
João Pedro Patrocinio <jpedro9966@gmail.com>
2020-12-14 14:13:30 -0300

    Sending route, controller and incomplete tests for jpedrodsp#14

--- maltese-backend/src/routes.js
+++ maltese-backend/src/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const ClientControllers = require("./controllers/ClientControllers");
 const ProductContollers = require("./controllers/ProductControllers");
+const ProviderController = require("./controllers/ProviderControllers");
 
 const routes = express.Router();
 
@@ -11,5 +12,12 @@ routes.post("/products", ProductContollers.store);
 routes.get("/products", ProductContollers.index);
 routes.put("/products/:product_id", ProductContollers.update);
 routes.delete("/products/:product_id", ProductContollers.delete);
+
+// Provider Routes
+routes.get("/provider/list", ProviderController.index);
+routes.get("/provider", ProviderController.read);
+routes.post("/provider", ProviderController.create);
+routes.put("/provider", ProviderController.update);
+routes.delete("/provider", ProviderController.delete);
 
 module.exports = routes;
